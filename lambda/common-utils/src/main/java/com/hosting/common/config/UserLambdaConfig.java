2eb6639165d9c665ba1b819afca1a2fc34f171fa
Jan <68644413+JanSkn@users.noreply.github.com>
2026-06-09 14:53:24 +0200

    fix(infra): wrong build architecture (#31)
    * fix(infra): missing permission to pull images
    
    * fix
    
    * fix
    
    * test: error linting
    
    * fix: delete block
    
    * fix: architecture
    
    * fix: linting

--- lambda/common-utils/src/main/java/com/hosting/common/config/UserLambdaConfig.java
+++ lambda/common-utils/src/main/java/com/hosting/common/config/UserLambdaConfig.java
@@ -10,6 +10,7 @@ public final class UserLambdaConfig {
   }
 
   public static final PackageType PACKAGE_TYPE = PackageType.IMAGE;
+  // architecture should match the codebuild architecture in the template.yml
   public static final Architecture ARCHITECTURE = Architecture.ARM64;
   public static final int TIMEOUT_SECONDS = 30;
   public static final int MEMORY_SIZE_MB = 128;
